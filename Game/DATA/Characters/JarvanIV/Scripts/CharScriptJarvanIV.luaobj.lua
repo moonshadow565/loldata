726a0424dd53df4aaa0c83844ed307d370447856
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 00:54:37 +0100

    live-0.0.0.17

---
+++ Game/DATA/Characters/JarvanIV/Scripts/CharScriptJarvanIV.luaobj.lua
@@ -0,0 +1,263 @@
+UpdateSelfBuffActionsBuildingBlocks = {
+  {
+    Function = BBGetStat,
+    Params = {
+      Stat = GetFlatPhysicalDamageMod,
+      TargetVar = "Owner",
+      DestVar = "BonusAD"
+    }
+  },
+  {
+    Function = BBMath,
+    Params = {
+      Src1Var = "BonusAD",
+      Src1Value = 0,
+      Src2Value = 1.1,
+      DestVar = "BonusAD2",
+      MathOp = MO_MULTIPLY
+    }
+  },
+  {
+    Function = BBMath,
+    Params = {
+      Src1Var = "BonusAD",
+      Src1Value = 0,
+      Src2Value = 1.5,
+      DestVar = "BonusAD",
+      MathOp = MO_MULTIPLY
+    }
+  },
+  {
+    Function = BBSetSpellToolTipVar,
+    Params = {
+      Value = 0,
+      ValueVar = "BonusAD2",
+      Index = 1,
+      SlotNumber = 0,
+      SlotType = SpellSlots,
+      SlotBook = SPELLBOOK_CHAMPION,
+      TargetVar = "Attacker"
+    }
+  },
+  {
+    Function = BBSetSpellToolTipVar,
+    Params = {
+      Value = 0,
+      ValueVar = "BonusAD",
+      Index = 1,
+      SlotNumber = 3,
+      SlotType = SpellSlots,
+      SlotBook = SPELLBOOK_CHAMPION,
+      TargetVar = "Attacker"
+    }
+  }
+}
+CharOnPreAttackBuildingBlocks = {
+  {
+    Function = BBIf,
+    Params = {Src1Var = "Target", CompareOp = CO_IS_TYPE_AI},
+    SubBlocks = {
+      {
+        Function = BBIf,
+        Params = {Src1Var = "Target", CompareOp = CO_IS_NOT_TURRET},
+        SubBlocks = {
+          {
+            Function = BBIfHasBuff,
+            Params = {
+              OwnerVar = "Target",
+              AttackerVar = "Attacker",
+              BuffName = "JarvanIVMartialCadenceCheck"
+            },
+            SubBlocks = {
+              {
+                Function = BBRemoveOverrideAutoAttack,
+                Params = {OwnerVar = "Owner", CancelAttack = true}
+              }
+            }
+          },
+          {
+            Function = BBElse,
+            Params = {},
+            SubBlocks = {
+              {
+                Function = BBOverrideAutoAttack,
+                Params = {
+                  SpellSlot = 0,
+                  SlotType = ExtraSlots,
+                  OwnerVar = "Owner",
+                  AutoAttackSpellLevel = 1,
+                  CancelAttack = true
+                }
+              }
+            }
+          }
+        }
+      },
+      {
+        Function = BBElse,
+        Params = {},
+        SubBlocks = {
+          {
+            Function = BBRemoveOverrideAutoAttack,
+            Params = {OwnerVar = "Owner", CancelAttack = true}
+          }
+        }
+      }
+    }
+  },
+  {
+    Function = BBElse,
+    Params = {},
+    SubBlocks = {
+      {
+        Function = BBRemoveOverrideAutoAttack,
+        Params = {OwnerVar = "Owner", CancelAttack = true}
+      }
+    }
+  }
+}
+CharOnActivateBuildingBlocks = {
+  {
+    Function = BBSpellBuffAdd,
+    Params = {
+      TargetVar = "Owner",
+      AttackerVar = "Owner",
+      BuffName = "APBonusDamageToTowers",
+      BuffAddType = BUFF_RENEW_EXISTING,
+      StacksExclusive = true,
+      BuffType = BUFF_Internal,
+      MaxStack = 1,
+      NumberOfStacks = 1,
+      Duration = 25000,
+      BuffVarsTable = "NextBuffVars",
+      TickRate = 0,
+      CanMitigateDuration = false,
+      IsHiddenOnClient = false
+    }
+  },
+  {
+    Function = BBSpellBuffAdd,
+    Params = {
+      TargetVar = "Owner",
+      AttackerVar = "Owner",
+      BuffName = "ChampionChampionDelta",
+      BuffAddType = BUFF_RENEW_EXISTING,
+      StacksExclusive = true,
+      BuffType = BUFF_Internal,
+      MaxStack = 1,
+      NumberOfStacks = 1,
+      Duration = 25000,
+      BuffVarsTable = "NextBuffVars",
+      TickRate = 0,
+      CanMitigateDuration = false,
+      IsHiddenOnClient = false
+    }
+  }
+}
+CharOnLevelUpSpellBuildingBlocks = {
+  {
+    Function = BBGetSlotSpellInfo,
+    Params = {
+      DestVar = "Level",
+      SpellSlotValue = 2,
+      SpellbookType = SPELLBOOK_CHAMPION,
+      SlotType = SpellSlots,
+      OwnerVar = "Owner",
+      Function = GetSlotSpellLevel
+    }
+  },
+  {
+    Function = BBSetVarInTable,
+    Params = {
+      DestVar = "ArmorBoost",
+      SrcValueByLevel = {
+        10,
+        4,
+        4,
+        4,
+        4
+      }
+    }
+  },
+  {
+    Function = BBSetVarInTable,
+    Params = {
+      DestVar = "AttackSpeedBoost",
+      SrcValueByLevel = {
+        0.1,
+        0.04,
+        0.04,
+        0.04,
+        0.04
+      }
+    }
+  },
+  {
+    Function = BBIf,
+    Params = {
+      Src1Var = "Slot",
+      Value2 = 2,
+      CompareOp = CO_EQUAL
+    },
+    SubBlocks = {
+      {
+        Function = BBIncPermanentStat,
+        Params = {
+          Stat = IncPermanentFlatArmorMod,
+          TargetVar = "Owner",
+          DeltaVar = "ArmorBoost",
+          Delta = 0
+        }
+      },
+      {
+        Function = BBIncPermanentStat,
+        Params = {
+          Stat = IncPermanentPercentAttackSpeedMod,
+          TargetVar = "Owner",
+          DeltaVar = "AttackSpeedBoost",
+          Delta = 0
+        }
+      }
+    }
+  }
+}
+CharOnDisconnectBuildingBlocks = {
+  {
+    Function = BBSpellCast,
+    Params = {
+      CasterVar = "Owner",
+      TargetVar = "Owner",
+      PosVar = "Owner",
+      EndPosVar = "Owner",
+      OverrideCastPosition = false,
+      SlotNumber = 6,
+      SlotType = InventorySlots,
+      OverrideForceLevel = 1,
+      OverrideCoolDownCheck = true,
+      FireWithoutCasting = false,
+      UseAutoAttackSpell = false,
+      ForceCastingOrChannelling = false,
+      UpdateAutoAttackTimer = false
+    }
+  }
+}
+PreLoadBuildingBlocks = {
+  {
+    Function = BBPreloadSpell,
+    Params = {
+      Name = "jarvanivmartialcadencecheck"
+    }
+  },
+  {
+    Function = BBPreloadSpell,
+    Params = {
+      Name = "apbonusdamagetotowers"
+    }
+  },
+  {
+    Function = BBPreloadSpell,
+    Params = {
+      Name = "championchampiondelta"
+    }
+  }
+}
