fe2621a84597705ee12f66b22298fddb6d0b0746
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 00:59:40 +0100

    live-0.0.0.112

---
+++ Game/DATA/Spells/ZyraPlantTaunted.luaobj.lua
@@ -0,0 +1,10 @@
+BuffTextureName = "GSB_taunt.dds"
+BuffName = "Taunt"
+PreLoadBuildingBlocks = {
+  {
+    Function = BBPreloadParticle,
+    Params = {
+      Name = "global_taunt.troy"
+    }
+  }
+}
