c15a57d4a4bde498e2bf7216160b1808548b5cfe
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:01:10 +0100

    live-0.0.0.138

--- Game/DATA/Characters/Nami/Spells/NamiQ.luaobj.lua
+++ Game/DATA/Characters/Nami/Spells/NamiQ.luaobj.lua
@@ -1,10 +1,7 @@
 AutoBuffActivateEffect = "maokai_sapling_activated_indicator.troy"
 AutoBuffActivateAttachBoneName = "C_BUFFBONE_GLB_CHEST_LOC"
 IsDeathRecapSource = true
-SpellFXOverrideSkins = {
-  "ZiggsMadScientist",
-  "ZiggsPoolParty"
-}
+SpellFXOverrideSkins = {"NamiKoi"}
 PreLoadBuildingBlocks = {
   {
     Function = BBPreloadCharacter,
