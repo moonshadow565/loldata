ba6661b7fc35938241d063382b905afb5445bcfb
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:40:24 +0100

    pbe-0.0.1.246

--- Game/DATA/Characters/Rengar/Spells/RengarESlow.luaobj.lua
+++ Game/DATA/Characters/Rengar/Spells/RengarESlow.luaobj.lua
@@ -2,11 +2,3 @@ BuffTextureName = "Chronokeeper_Timestop.dds"
 BuffName = "Slow"
 AutoBuffActivateEffect = "GLOBAL_SLOW.TROY"
 PopupMessage1 = "game_floatingtext_Slowed"
-PreLoadBuildingBlocks = {
-  {
-    Function = BBPreloadParticle,
-    Params = {
-      Name = "rengar_base_e_tar.troy"
-    }
-  }
-}
