414a540fdd7a488e0ae566f9f60bee08cbb76d2f
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:44:50 +0100

    pbe-0.0.3.7

---
+++ Game/DATA/Characters/RekSai/Spells/RekSaiTunnelTime.luaobj.lua
@@ -0,0 +1,20 @@
+NotSingleTargetSpell = false
+DoesntBreakShields = true
+DoesntTriggerSpellCasts = true
+CastingBreaksStealth = true
+IsDamagingSpell = true
+BuffName = "RekSaiTunnelTime"
+ChainMissileParameters = {
+  MaximumHits = {
+    10,
+    10,
+    10,
+    10,
+    10
+  },
+  CanHitCaster = 0,
+  CanHitSameTarget = 0,
+  CanHitSameTargetConsecutively = 0,
+  CanHitEnemies = 1,
+  CanHitFriends = 0
+}
