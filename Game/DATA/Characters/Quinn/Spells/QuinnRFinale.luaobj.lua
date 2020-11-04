427acb8ce34f0ef5be33d9fb008ab1b42af608b7
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:49:04 +0100

    pbe-0.0.3.233

--- Game/DATA/Characters/Quinn/Spells/QuinnRFinale.luaobj.lua
+++ Game/DATA/Characters/Quinn/Spells/QuinnRFinale.luaobj.lua
@@ -2,8 +2,4 @@ NotSingleTargetSpell = true
 DoesntBreakShields = false
 DoesntTriggerSpellCasts = false
 CastingBreaksStealth = true
-SpellFXOverrideSkins = {
-  "PhoenixQuinn",
-  "QuinnSkin02"
-}
 IsDamagingSpell = true
