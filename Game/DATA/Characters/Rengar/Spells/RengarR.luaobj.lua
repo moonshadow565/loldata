22f57492afdf2fdb3a5502e72a2a565e42c7d696
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:39:44 +0100

    pbe-0.0.1.183

--- Game/DATA/Characters/Rengar/Spells/RengarR.luaobj.lua
+++ Game/DATA/Characters/Rengar/Spells/RengarR.luaobj.lua
@@ -4,7 +4,6 @@ CastingBreaksStealth = true
 IsDamagingSpell = false
 BuffTextureName = "RengarUlt.dds"
 BuffName = "RengarRBuff"
-AutoBuffActivateEffect = "Global_haste.troy"
 SpellToggleSlot = 1
 SpellFXOverrideSkins = {
   "HunterRengar"
---
+++ Game/DATA/Characters/Rengar/Spells/RengarRSpeed.luaobj.lua
@@ -0,0 +1,11 @@
+NotSingleTargetSpell = true
+DoesntTriggerSpellCasts = false
+CastingBreaksStealth = true
+IsDamagingSpell = false
+BuffTextureName = "RengarUlt.dds"
+BuffName = "RengarRBuff"
+AutoBuffActivateEffect = "Global_haste.troy"
+SpellToggleSlot = 1
+SpellFXOverrideSkins = {
+  "HunterRengar"
+}
