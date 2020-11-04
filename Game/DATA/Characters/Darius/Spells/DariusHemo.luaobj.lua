96007a449284bac63ee1b84dfd3cb82189a8c916
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:46:58 +0100

    pbe-0.0.3.123

--- Game/DATA/Characters/Darius/Spells/DariusHemo.luaobj.lua
+++ Game/DATA/Characters/Darius/Spells/DariusHemo.luaobj.lua
@@ -2,4 +2,3 @@ DoesntTriggerSpellCasts = true
 BuffTextureName = "Darius_Icon_Hemorrhage.dds"
 BuffName = "DariusHemo"
 IsDeathRecapSource = true
-PersistsThroughDeath = true
--- Game/DATA/Characters/Mordekaiser/Spells/MordekaiserChildrenOfTheGrave.luaobj.lua
+++ Game/DATA/Characters/Mordekaiser/Spells/MordekaiserChildrenOfTheGrave.luaobj.lua
@@ -1,3 +1,4 @@
 BuffTextureName = "Mordekaiser_COTG.dds"
 BuffName = "MordekaiserCOTGDot"
+NonDispellable = true
 OnPreDamagePriority = 10
