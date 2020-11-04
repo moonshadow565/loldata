15acca2f34dbf68ed62804608ef5b51846d03ad2
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:36:13 +0100

    pbe-0.0.0.217

--- Game/DATA/Spells/DariusExecuteMulticast.luaobj.lua
+++ Game/DATA/Spells/DariusExecuteMulticast.luaobj.lua
@@ -1,7 +1,5 @@
 BuffTextureName = "Darius_Icon_Sudden_Death.dds"
 BuffName = "DariusExecuteMulticast"
-AutoBuffActivateEffect = "bloodboil_buf.troy"
-AutoBuffActivateAttachBoneName = "l_hand"
-AutoBuffActivateEffect2 = "bloodboil_buf.troy"
-AutoBuffActivateAttachBoneName2 = "r_hand"
+AutoBuffActivateEffect = "Darius_R_Ready.troy"
+AutoBuffActivateAttachBoneName = "buffbone_cstm_weapon_1"
 PersistsThroughDeath = true
