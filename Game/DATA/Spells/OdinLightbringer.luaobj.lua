6486f612dc14da2430b0dbd1654eb5a6efcd601f
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:35:29 +0100

    pbe-0.0.0.149

--- Game/DATA/Spells/OdinLightbringer.luaobj.lua
+++ Game/DATA/Spells/OdinLightbringer.luaobj.lua
@@ -1,4 +1,4 @@
-BuffTextureName = "Nidalee_OnTheProwl.dds"
+BuffTextureName = "Odin_Lightbringer.dds"
 BuffName = "OdinLightbringer"
 AutoBuffActivateEffect = "global_Watched.troy"
 AutoBuffActivateAttachBoneName = "head"
