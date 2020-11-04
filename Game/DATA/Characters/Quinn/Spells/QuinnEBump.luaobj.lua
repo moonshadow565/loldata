8c5d71c5543987032d405530483e9c2b95ae9fb0
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:36:34 +0100

    pbe-0.0.0.248

---
+++ Game/DATA/Characters/Quinn/Spells/QuinnEBump.luaobj.lua
@@ -0,0 +1,15 @@
+NotSingleTargetSpell = false
+DoesntBreakShields = false
+DoesntTriggerSpellCasts = false
+IsDamagingSpell = true
+BuffTextureName = "Quinn_E.dds"
+BuffName = "QuinnERoot"
+AutoCooldownByLevel = {
+  12,
+  11,
+  10,
+  9,
+  8
+}
+IsDeathRecapSource = true
+PopupMessage1 = "game_floatingtext_Slowed"
