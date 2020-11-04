b11545dd8703c7ab799960c97e80cb11c10b001d
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:42:41 +0100

    pbe-0.0.2.150

--- Game/DATA/Spells/LuxIlluminatingFraulein.luaobj.lua
+++ Game/DATA/Spells/LuxIlluminatingFraulein.luaobj.lua
@@ -1,7 +1,7 @@
 BuffTextureName = "LuxIlluminatingFraulein.dds"
 BuffName = "LuxIlluminatingFraulein"
 AutoBuffActivateEffect = "LuxDebuff.troy"
-AutoBuffActivateAttachBoneName = "C_Buffbone_GLb_Chest_Loc"
+AutoBuffActivateAttachBoneName = "C_Buffbone_GLb_Head_Loc"
 SpellFXOverrideSkins = {
   "SteelLegionLux"
 }
---
+++ Game/DATA/Spells/MegaAdhesiveSnare.luaobj.lua
@@ -0,0 +1,2 @@
+BuffTextureName = "ChemMan_MegaAdhesive.dds"
+PopupMessage1 = "game_floatingtext_Rooted"
