b1c23c16556d110e6f312f7a60727c0ddbd81508
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:07:40 +0100

    live-0.0.0.238

--- Game/DATA/Characters/Draven/Spells/DravenBasicAttack.luaobj.lua
+++ Game/DATA/Characters/Draven/Spells/DravenBasicAttack.luaobj.lua
@@ -4,3 +4,41 @@ SpellFXOverrideSkins = {
 SpellVOOverrideSkins = {
   "Draven_SoulReaper"
 }
+PreLoadBuildingBlocks = {
+  {
+    Function = BBPreloadSpell,
+    Params = {
+      Name = "dravenattackp_lq"
+    }
+  },
+  {
+    Function = BBPreloadSpell,
+    Params = {
+      Name = "dravenattackp_rq"
+    }
+  },
+  {
+    Function = BBPreloadSpell,
+    Params = {
+      Name = "dravenattackp_r"
+    }
+  },
+  {
+    Function = BBPreloadSpell,
+    Params = {
+      Name = "dravenspinningonehand"
+    }
+  },
+  {
+    Function = BBPreloadSpell,
+    Params = {
+      Name = "dravenspinningreturn"
+    }
+  },
+  {
+    Function = BBPreloadSpell,
+    Params = {
+      Name = "dravenspinningleft"
+    }
+  }
+}
