80d443ec7ebf89d30aa1abd5e8cc75bd082e9ad3
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:34:42 +0100

    pbe-0.0.0.60

--- Game/DATA/Items/3047.luaobj.lua
+++ Game/DATA/Items/3047.luaobj.lua
@@ -1,10 +1,49 @@
-UpdateSelfBuffStatsBuildingBlocks = {
+UpdateSelfBuffActionsBuildingBlocks = {
   {
-    Function = BBSetStatus,
+    Function = BBExecutePeriodically,
     Params = {
-      TargetVar = "Owner",
-      SrcValue = true,
-      Status = SetGhosted
+      TimeBetweenExecutions = 10,
+      TrackTimeVar = "LastTimeExecuted",
+      TrackTimeVarTable = "InstanceVars",
+      ExecuteImmediately = true
+    },
+    SubBlocks = {
+      {
+        Function = BBSpellBuffAdd,
+        Params = {
+          TargetVar = "Owner",
+          AttackerVar = "Owner",
+          BuffName = "3047",
+          BuffAddType = BUFF_RENEW_EXISTING,
+          StacksExclusive = true,
+          BuffType = BUFF_Internal,
+          MaxStack = 1,
+          NumberOfStacks = 1,
+          Duration = 11,
+          BuffVarsTable = "NextBuffVars",
+          TickRate = 0,
+          CanMitigateDuration = false,
+          IsHiddenOnClient = false
+        }
+      }
+    }
+  }
+}
+BuffOnBeingHitBuildingBlocks = {
+  {
+    Function = BBIf,
+    Params = {Value1 = DAMAGESOURCE_ATTACK, CompareOp = CO_DAMAGE_SOURCETYPE_IS},
+    SubBlocks = {
+      {
+        Function = BBMath,
+        Params = {
+          Src1Var = "DamageAmount",
+          Src1Value = 0,
+          Src2Value = 0.9,
+          DestVar = "DamageAmount",
+          MathOp = MO_MULTIPLY
+        }
+      }
     }
   }
 }
--- Game/DATA/Spells/LightningRodApplicator.luaobj.lua
+++ Game/DATA/Spells/LightningRodApplicator.luaobj.lua
@@ -1,4 +1,3 @@
-PersistsThroughDeath = true
 OnBuffActivateBuildingBlocks = {
   {
     Function = BBSetVarInTable,
