065197d5285efb80ffae9ae72439eb3f7ec63603
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:38:44 +0100

    pbe-0.0.1.102

---
+++ Game/DATA/Spells/NPCInitializer.luaobj.lua
@@ -0,0 +1,14 @@
+PreLoadBuildingBlocks = {
+  {
+    Function = BBPreloadSpell,
+    Params = {
+      Name = "npcspawnmanager"
+    }
+  },
+  {
+    Function = BBPreloadSpell,
+    Params = {
+      Name = "npcaggromanager"
+    }
+  }
+}
