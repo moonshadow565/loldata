49c391a519cddc9e0a87ffbcd0ae86d2b0217980
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:48:36 +0100

    pbe-0.0.3.209

---
+++ Game/DATA/Spells/BilgewaterCutlassDebuff.luaobj.lua
@@ -0,0 +1,6 @@
+NotSingleTargetSpell = true
+DoesntTriggerSpellCasts = true
+CastingBreaksStealth = true
+BuffTextureName = "3144_Bilgewater_Cutlass.dds"
+BuffName = "BilgewaterCutlass"
+PopupMessage1 = "game_floatingtext_Slowed"
