4532f6a754434810b6545daa8a4746a3bdd27e91
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:43:28 +0100

    pbe-0.0.2.191

--- Game/DATA/Spells/ItemVoidGate.luaobj.lua
+++ Game/DATA/Spells/ItemVoidGate.luaobj.lua
@@ -1,10 +1,3 @@
 NotSingleTargetSpell = true
 DoesntTriggerSpellCasts = false
 IsDamagingSpell = true
-SpellDamageRatio = 0.5
-IsPetDurationBuff = true
-function UpdateBuffs(A0_0, A1_1, A2_2, A3_3)
-  if A2_2 < 1 then
-    SetScaleSkinCoef(A2_2, A1_1)
-  end
-end
