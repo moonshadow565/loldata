7f0dddbafa03a2f949328d70e8f5f3be0261870d
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:43:35 +0100

    pbe-0.0.2.198

---
+++ Game/LEVELS/map11/Scripts/Mutators/NightmareBotDifficulty.luaobj.lua
@@ -0,0 +1,38 @@
+local L0, L1
+L0 = 100
+TEAM_ORDER = L0
+L0 = 200
+TEAM_CHAOS = L0
+L0 = 1
+DIFFICULTY_INDEX = L0
+function L0()
+  local L0, L1
+end
+OnInitClient = L0
+function L0(A0)
+  local L1, L2, L3
+  L2 = A0
+  L1 = A0.GetFloat
+  L1 = L1(L2)
+  DIFFICULTY_INDEX = L1
+  L1 = LuaForEachChampion
+  L2 = TEAM_ORDER
+  L3 = "SetNightmareBotDifficulty"
+  L1(L2, L3)
+  L1 = LuaForEachChampion
+  L2 = TEAM_CHAOS
+  L3 = "SetNightmareBotDifficulty"
+  L1(L2, L3)
+end
+OnInitServer = L0
+function L0(A0)
+  local L1, L2, L3, L4, L5, L6
+  L1 = ApplyPersistentBuff
+  L2 = A0
+  L3 = "NightmareBotDifficulty"
+  L4 = false
+  L5 = DIFFICULTY_INDEX
+  L6 = 3
+  L1(L2, L3, L4, L5, L6)
+end
+SetNightmareBotDifficulty = L0
