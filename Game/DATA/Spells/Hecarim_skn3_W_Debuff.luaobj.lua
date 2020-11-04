95041c79e418ce65572360dd35ded6e676964e24
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:38:26 +0100

    pbe-0.0.1.74

--- Game/DATA/Spells/Hecarim_skn3_W_Debuff.luaobj.lua
+++ Game/DATA/Spells/Hecarim_skn3_W_Debuff.luaobj.lua
@@ -4,5 +4,11 @@ PreLoadBuildingBlocks = {
     Params = {
       Name = "hecarim_skn4_w_debuff.troy"
     }
+  },
+  {
+    Function = BBPreloadSpell,
+    Params = {
+      Name = "hecarim_skn3_w_debuff"
+    }
   }
 }
