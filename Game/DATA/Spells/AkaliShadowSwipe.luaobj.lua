8fb387759a56acc9f4ac04c640122a7975073f93
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:37:08 +0100

    pbe-0.0.1.44

--- Game/DATA/Spells/AkaliShadowSwipe.luaobj.lua
+++ Game/DATA/Spells/AkaliShadowSwipe.luaobj.lua
@@ -4,6 +4,12 @@ IsDamagingSpell = true
 SpellDamageRatio = 0.5
 PreLoadBuildingBlocks = {
   {
+    Function = BBPreloadSpell,
+    Params = {
+      Name = "katarinaqmarkspellshieldcheck"
+    }
+  },
+  {
     Function = BBPreloadParticle,
     Params = {
       Name = "akali_mark_impact_tar.troy"
