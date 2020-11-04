8488d640733b6773a91ab3e3fb0f432cf8fa2907
moonshadow565 <moonshadow565@hotmail.com>
2020-11-04 01:38:33 +0100

    pbe-0.0.1.86

--- Game/DATA/Characters/Nidalee/Spells/BushwhackDamage.luaobj.lua
+++ Game/DATA/Characters/Nidalee/Spells/BushwhackDamage.luaobj.lua
@@ -1,7 +1,5 @@
 BuffTextureName = "NIdalee_Bushwhack.dds"
 BuffName = "BushwhackDamage"
-AutoBuffActivateEffect = "global_Watched.troy"
-AutoBuffActivateAttachBoneName = "C_BUFFBONE_GLB_HEAD_LOC"
 SpellFXOverrideSkins = {
   "HeadhunterNidalee"
 }
--- Game/DATA/Characters/Nidalee/Spells/BushwhackDebuff.luaobj.lua
+++ Game/DATA/Characters/Nidalee/Spells/BushwhackDebuff.luaobj.lua
@@ -5,6 +5,8 @@ CastingBreaksStealth = false
 IsDamagingSpell = true
 BuffTextureName = "NIdalee_Bushwhack.dds"
 BuffName = "Bushwhack"
+AutoBuffActivateEffect = "global_Watched.troy"
+AutoBuffActivateAttachBoneName = "C_BUFFBONE_GLB_HEAD_LOC"
 SpellFXOverrideSkins = {
   "HeadhunterNidalee"
 }
